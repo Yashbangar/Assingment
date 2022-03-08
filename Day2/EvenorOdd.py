4e5d955e5ed89816c664d4ae7e11adab5648d11a
Yashbangar <99308632+Yashbangar@users.noreply.github.com>
2022-03-08 23:16:55 +0530

    Delete Day2 directory

--- Day2/EvenorOdd.py
+++
@@ -1,6 +0,0 @@
-# Q5. Write a program to check whether a number entered by user is even or odd.
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
-else:
-   print("{0} is Odd".format(num))
